db35d6ec4f00f68990ad3e1369f7301f1f0ef5a8
Heifetz <Seraph_90@mail.ru>
2018-11-14 16:36:32 +0300

    Автообновление виджета каждые 30 секунд

--- app/src/main/java/com/heifetz/heifetz/LentaWidget.kt
+++ app/src/main/java/com/heifetz/heifetz/LentaWidget.kt
@@ -8,13 +8,30 @@ import android.widget.RemoteViews
 import com.heifetz.heifetz.enums.Stops
 import com.heifetz.heifetz.helpers.DBHelper
 import com.heifetz.heifetz.helpers.coloringTimes
+import java.util.*
 
 class LentaWidget : AppWidgetProvider() {
 
-    private lateinit var dbHelper: DBHelper
+    private val timer = Timer()
+
+    private fun startTimer(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                if (appWidgetIds != null) {
+                    for (appWidgetId in appWidgetIds) {
+                        updateAppWidget(context, appWidgetManager, appWidgetId)
+                    }
+                }
+            }
+        }, 0, 30000)
+    }
+
+    private fun stopTimer() {
+        timer.cancel()
+    }
 
     private fun updateAppWidget(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetId: Int) {
-        dbHelper = DBHelper(context!!)
+        val dbHelper = DBHelper(context!!)
 
         val times = coloringTimes(dbHelper.getSortedTimes(Stops.START))
 
@@ -42,14 +59,14 @@ class LentaWidget : AppWidgetProvider() {
         appWidgetManager?.updateAppWidget(appWidgetId, views)
     }
 
+    override fun onDisabled(context: Context?) {
+        super.onDisabled(context)
+        stopTimer()
+    }
+
     override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
-
-        if (appWidgetIds != null) {
-            for (appWidgetId in appWidgetIds) {
-                updateAppWidget(context, appWidgetManager, appWidgetId)
-            }
-        }
+        startTimer(context, appWidgetManager, appWidgetIds)
     }
 
 }
